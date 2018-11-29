487078c6a6272a368937498fc544ae50a58a796c
shrirammoorthy <duke.shriram@gmail.com>
2018-11-29 12:53:41 +0530

    todo app

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">HOME</div>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are successfully logged in! Manage your ToDo and Make life easy!! (see under dropdown)
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
